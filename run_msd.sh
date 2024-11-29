a1b27b93a4f150989c7838afc01f397b84243135
constracktor <strack.as@icloud.com>
2024-11-29 01:29:03 +0100

    Remove requirements file

--- run_msd.sh
+++ run_msd.sh
@@ -4,6 +4,6 @@ python3 -m venv msd_env
 # activate enviroment
 source msd_env/bin/activate
 # install requirements
-pip install --no-cache-dir -r requirements.txt
+pip install --no-cache-dir numpy scipy matplotlib
 # launch 
 python3 generate_msd_data.py
